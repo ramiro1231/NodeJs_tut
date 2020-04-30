df4478ac663507035374152e35047f87fd7738b5
Ramiro Alvarez <ramiro89011@gmail.com>
2020-04-29 22:26:08 -0500

    Agrego funciones d eacceso

--- movies-api/lib/mongo.js
+++ movies-api/lib/mongo.js
@@ -12,23 +12,53 @@ class MongoLib {
         this.client = new MongoClient(MONGO_URI, { useNewUrlParser: true });
         this.dbName = DB_NAME;
     }
-}
+    connect() {
+        if (!MongoLib.connection) {
+            MongoLib.connection = new Promise((resolve, reject) => {
+                this.client.connect(err => {
+                    if (err) {
+                        reject(err);
+                    }
 
-connect() {
-    if (!MongoLib.connection) {
-        MongoLib.connection = new Promise((resolve, reject) => {
-            this.client.connect(err => {
-                if (err) {
-                    reject(err);
-                }
-
-                console.log('Connected sucesufully to mongo');
-                resolve(this.client.db(this.dbName))
+                    console.log('Connected sucesufully to mongo');
+                    resolve(this.client.db(this.dbName));
+                });
             });
-        });
+        }
+
+        return MongoLib.connection;
     }
 
-    return MongoLib.connection;
+    getAll(collection, query) {
+        return this.connect().then(db => {
+            return db.collection(collection.find(query).toArray());
+        })
+    }
+
+    get(collection, id) {
+        return this.connect().then(db => {
+            return db.collection(collection.findOne({ _id: ObjectId(id) }));
+        })
+    }
+
+    create(collection, data) {
+        return this.connect().then(db => {
+            return db.collection(collection.insertOne(data));
+        }).then(result => result.insertId)
+    }
+
+    update(collection, id, data) {
+        return this.connect().then(db => {
+            return db.collection(collection.updateOne({ _id: ObjectId(id) }), { $set: data }, { upsert: true });
+        }).then(result => result.upseredId || id)
+    }
+    delete(collection, id) {
+        return this.connect().then(db => {
+            return db.collection(collection.deleteOne({ _id: ObjectId(id) }));
+        }).then(() => id)
+    }
 }
+
+
 
 module.exports = MongoLib;
